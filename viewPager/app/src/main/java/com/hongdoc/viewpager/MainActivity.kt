6b26830cd1f9d196655b104faec0dc46514c8179
hong_doc <holives7@gmail.com>
2021-10-02 17:56:38 +0900

    4

---
+++ viewPager/app/src/main/java/com/hongdoc/viewpager/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.hongdoc.viewpager
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hongdoc.viewpager.databinding.ActivityMainBinding
+
+class MainActivity : FragmentActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val fragmentList = listOf(FragmentA(), FragmentB(), FragmentC(), FragmentD())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        binding.viewPager.adapter = adapter
+
+        val tabTitles = listOf<String>("A", "B", "C", "D")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
+}
