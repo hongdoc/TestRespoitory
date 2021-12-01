fc5d86e48bd8298f7f908727a017ddaca66821bc
hong_doc <holives7@gmail.com>
2021-12-01 14:16:11 +0900

    트로트 가수 음악 목록 앱 만들기 리사이클러뷰 이용하여 완성

--- trot_list/app/src/main/java/com/hongdoc/trot_list/Singer2Fragment.kt
+++ trot_list/app/src/main/java/com/hongdoc/trot_list/Singer2Fragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Singer2Fragment : Fragment() {
 
@@ -21,6 +23,34 @@ class Singer2Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_singer2, container, false)
+
+        val items = mutableListOf<String>()
+        items.add("별빛 같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+        items.add("별빛 같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+        items.add("별빛 같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+        items.add("별빛 같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+        items.add("별빛 같은 나의 사랑")
+        items.add("사랑의 콜센터")
+        items.add("영웅시대")
+        items.add("이제 나만 믿어요")
+
+        val rv = view.findViewById<RecyclerView>(R.id.singRV)
+        val rvAdapter = RVAdapter(items)
+
+        rv.adapter = rvAdapter
+        rv.layoutManager = LinearLayoutManager(context)
 
         view.findViewById<ImageView>(R.id.image1).setOnClickListener {
             it.findNavController().navigate(R.id.action_singer2Fragment_to_singer1Fragment)
