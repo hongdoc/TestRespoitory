0cde25cbce0448c9fbd538418d85827a1dbe87b3
hong_doc <holives7@gmail.com>
2021-12-06 01:56:18 +0900

    Firebase RealTiem DataBase 불러오기

---
+++ diet_memo/app/src/main/java/com/hongdoc/diet_memo/DataModel.kt
@@ -0,0 +1,7 @@
+package com.hongdoc.diet_memo
+
+data class DataModel (
+
+    val date : String = "",
+    val memo : String = ""
+)
--- diet_memo/app/src/main/java/com/hongdoc/diet_memo/MainActivity.kt
+++ diet_memo/app/src/main/java/com/hongdoc/diet_memo/MainActivity.kt
@@ -5,17 +5,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.ImageView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val dataModelList = mutableListOf<DataModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("myMemo")
+
+        val listView = findViewById<ListView>(R.id.mainLV)
+
+        val adapter_list = ListViewAdapter(dataModelList)
+
+        listView.adapter = adapter_list
+
+        //Firebase에서 Data 불러온다.
+        myRef.child(Firebase.auth.currentUser!!.uid).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                dataModelList.clear()
+
+                for (dataModel in snapshot.children){
+                    Log.d("Data", dataModel.toString())
+                    dataModelList.add(dataModel.getValue(DataModel::class.java)!!)
+                }
+                // 데이터 모델에 값이 다 들어가면 Adapter 새롭게 만들어주는 코드.
+                adapter_list.notifyDataSetChanged()
+                Log.d("DataModel", dataModelList.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
 
         val writeButton = findViewById<ImageView>(R.id.writeBtn)
         writeButton.setOnClickListener {
@@ -50,6 +86,25 @@ class MainActivity : AppCompatActivity() {
 
                 }, year, month, date)
                 dlg.show()
+
+            }
+
+            val saveBtn = mAlertDialog.findViewById<Button>(R.id.SaveBtn)
+            saveBtn?.setOnClickListener {
+
+                val healMemo = mAlertDialog.findViewById<EditText>(R.id.healthMemo)?.text.toString()
+
+                val database = Firebase.database
+                val myRef = database.getReference("myMemo").child(Firebase.auth.currentUser!!.uid)
+
+                val model = DataModel(dateText, healMemo)
+
+                myRef
+                    .push()
+                    .setValue(model)
+
+                mAlertDialog.dismiss()
+
 
             }
         }
