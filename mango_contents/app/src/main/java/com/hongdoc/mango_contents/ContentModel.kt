e089850e75e5ce0fbce31db72f4597665a76ff8e
hong_doc <holives7@gmail.com>
2021-12-07 14:11:28 +0900

    망코플레이트 앱 만들기

---
+++ mango_contents/app/src/main/java/com/hongdoc/mango_contents/ContentModel.kt
@@ -0,0 +1,9 @@
+package com.hongdoc.mango_contents
+
+data class ContentModel (
+
+    val url : String = "",
+    val titleImageUrl : String = "",
+    val titleText : String = ""
+
+)
---
+++ mango_contents/app/src/main/java/com/hongdoc/mango_contents/RVAdapter.kt
@@ -0,0 +1,31 @@
+package com.hongdoc.mango_contents
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAdapter(val List : MutableList<ContentModel>) : RecyclerView.Adapter<RVAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.rv_item, parent, false)
+
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RVAdapter.ViewHolder, position: Int) {
+        holder.bindItems(List[position])
+    }
+
+    override fun getItemCount(): Int {
+        return List.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        fun bindItems(item : ContentModel){
+
+        }
+    }
+
+}
