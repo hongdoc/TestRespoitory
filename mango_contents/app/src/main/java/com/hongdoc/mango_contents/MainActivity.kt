83bbc11efa94ce539c240a80cbe5e5fa85b54761
hong_doc <holives7@gmail.com>
2021-12-09 01:15:23 +0900

    망고 플레이트 앱만들기 RecyclerView 꾸미기

--- mango_contents/app/src/main/java/com/hongdoc/mango_contents/MainActivity.kt
+++ mango_contents/app/src/main/java/com/hongdoc/mango_contents/MainActivity.kt
@@ -24,9 +24,37 @@ import androidx.recyclerview.widget.RecyclerView
         )
         items.add(
             ContentModel(
-                "https://www.mangoplate.com/restaurants/l0JdGHGMmsel",
-                "https://mp-seoul-image-production-s3.mangoplate.com/872233_1595994610070732.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "스시요로코부"
+                "https://www.mangoplate.com/restaurants/0Dc83nt2DQsa",
+                "https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/593796_1471281207718379.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "다정도병인양"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/8_V151r3Zgqe",
+                "https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/584479_1479740575442069.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "유키가후루"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/yT6tRfkdamSG",
+                "https://mp-seoul-image-production-s3.mangoplate.com/231804/627630_1529537247599_108676?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "춘천막국수"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/AiPzUgBBHN",
+                "https://mp-seoul-image-production-s3.mangoplate.com/14226/855261_1623556447640_8972?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "대보명가"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/Zy0b5FFvV_",
+                "https://mp-seoul-image-production-s3.mangoplate.com/187830/1528015_1613477437974_3894?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "히피스베이글"
             )
         )
         items.add(
@@ -34,6 +62,41 @@ import androidx.recyclerview.widget.RecyclerView
                 "https://www.mangoplate.com/restaurants/l0JdGHGMmsel",
                 "https://mp-seoul-image-production-s3.mangoplate.com/872233_1595994610070732.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
                 "스시요로코부"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/0Dc83nt2DQsa",
+                "https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/593796_1471281207718379.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "다정도병인양"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/8_V151r3Zgqe",
+                "https://mp-seoul-image-production-s3.mangoplate.com/added_restaurants/584479_1479740575442069.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "유키가후루"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/yT6tRfkdamSG",
+                "https://mp-seoul-image-production-s3.mangoplate.com/231804/627630_1529537247599_108676?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "춘천막국수"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/AiPzUgBBHN",
+                "https://mp-seoul-image-production-s3.mangoplate.com/14226/855261_1623556447640_8972?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "대보명가"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/Zy0b5FFvV_",
+                "https://mp-seoul-image-production-s3.mangoplate.com/187830/1528015_1613477437974_3894?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "히피스베이글"
             )
         )
 
