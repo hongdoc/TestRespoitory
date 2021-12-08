b95109459a9ac2a38e3c8ee245570784f26a90f0
hong_doc <holives7@gmail.com>
2021-12-08 13:18:36 +0900

    망고 플레이트 앱만들기1.1

--- mango_contents/app/src/main/java/com/hongdoc/mango_contents/MainActivity.kt
+++ mango_contents/app/src/main/java/com/hongdoc/mango_contents/MainActivity.kt
@@ -2,13 +2,46 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+
+    private val items = mutableListOf<ContentModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/l0JdGHGMmsel",
+                "https://mp-seoul-image-production-s3.mangoplate.com/872233_1595994610070732.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+            "스시요로코부"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/l0JdGHGMmsel",
+                "https://mp-seoul-image-production-s3.mangoplate.com/872233_1595994610070732.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "스시요로코부"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/l0JdGHGMmsel",
+                "https://mp-seoul-image-production-s3.mangoplate.com/872233_1595994610070732.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "스시요로코부"
+            )
+        )
+
+        val recyclerview = findViewById<RecyclerView>(R.id.rv)
+        val rvAdapter = RVAdapter(baseContext, items)
+        recyclerview.adapter = rvAdapter
+
+        recyclerview.layoutManager = GridLayoutManager(this, 2)
 
     }
 
--- mango_contents/app/src/main/java/com/hongdoc/mango_contents/RVAdapter.kt
+++ mango_contents/app/src/main/java/com/hongdoc/mango_contents/RVAdapter.kt
@@ -1,12 +1,16 @@
 package com.hongdoc.mango_contents
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class RVAdapter(val List : MutableList<ContentModel>) : RecyclerView.Adapter<RVAdapter.ViewHolder>() {
+class RVAdapter(val context : Context, val List : MutableList<ContentModel>) : RecyclerView.Adapter<RVAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.rv_item, parent, false)
 
@@ -24,6 +28,16 @@ class RVAdapter(val List : MutableList<ContentModel>) : RecyclerView.Adapter<RVA
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
         fun bindItems(item : ContentModel){
+
+            val rv_img = itemView.findViewById<ImageView>(R.id.rvImageArea)
+            val rv_text = itemView.findViewById<TextView>(R.id.rvTextArea)
+
+            rv_text.text = item.titleText
+            // context는 현재 상태의 맥락
+            Glide.with(context)
+                .load(item.titleImageUrl)
+                .into(rv_img)
+
 
         }
     }
