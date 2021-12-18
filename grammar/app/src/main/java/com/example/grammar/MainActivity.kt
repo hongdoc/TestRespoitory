210194b84ad939d8afec9f4760d1c4072cde31e6
hong_doc <holives7@gmail.com>
2021-12-18 23:24:36 +0900

    문법 공부

--- grammar/app/src/main/java/com/example/grammar/MainActivity.kt
+++ grammar/app/src/main/java/com/example/grammar/MainActivity.kt
@@ -408,3 +408,35 @@ fun remainder(num1 : Int, num2 : Int){
 	fun findStringLength4(str : String?) : Int {
         return str?.length ?: 0
         */
+
+
+
+//Any
+//이름처럼, 아무거나 할 수 있는 느낌?
+//모든 아이들을 포함하는 조상같은 친구?
+//
+//is as
+// is -> 맞나? 확인
+// as -> type casting -> 타입 변경 체크
+
+    /*
+    var str1 : String = "ABC"
+    println(str1)
+    str1 = 123
+    println(str1)
+
+	var str2 : Any = "abc"
+    println(str2)
+    str2 = 123
+    println(str2)
+    */
+
+    var str3 : Any = "abc"
+
+    if(str3 is String){
+        println("this is string")
+    } else {
+    	println("this is not String")
+    }
+
+   */
