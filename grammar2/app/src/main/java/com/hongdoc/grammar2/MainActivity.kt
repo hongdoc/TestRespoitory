e12e65033a8d28bf2ac78c9ba0b4a34652820443
hong_doc <holives7@gmail.com>
2021-12-23 13:27:06 +0900

    문법공부2 상속

--- grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
+++ grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
@@ -46,3 +46,49 @@ fun sumNumber(a : String, b : String){
     println(a+b)
 }
 */
+
+
+
+/*
+    //상속
+    //Inheritance
+    //상속 -> 물려받는다??
+    
+    Job1()
+    Job2()
+    Job3()
+    Job4()
+}
+//상속
+open class AllJobs(){
+    init{
+        println("일을 합니다.")
+    }
+}
+
+class Job3() : AllJobs(){
+	init {
+        println("마케팅을 합니다.")
+    }
+}
+
+class Job4() : AllJobs(){
+	init{
+        println("근로를 합니다.")
+    }
+}
+
+//일반적으로 했을 때
+class Job1(){
+    init {
+        println("일을 한다")
+        println("코딩을 한다")
+    }
+}
+    class Job2(){
+    init {
+        println("일을 한다")
+        println("디자인을 한다")
+    }
+}
+    */
