15dea649c5a50e266563eccdd68c02cc1790b1eb
hong_doc <holives7@gmail.com>
2021-12-26 20:46:13 +0900

    문법공부 중첩클래스, 내부클래스

--- grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
+++ grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
@@ -120,3 +120,46 @@ data class DataDog(var name : String, var age : Int)[
 
 ]
 */
+
+/*
+    //중첩 클래스(Nested class)
+    //내부 클래스(Inner class)
+    //
+    // 중첩 클래스 -> 객쳊디향 / 캡슐화
+    // 내부 클래스 -> RecyclerView
+    val test1 = Test1.Test1NestedClass()
+    test1.testFun1()
+
+    val test2 = Test2().Test2InnerClass()
+    test2.testFun2()
+}
+
+class Test1 {
+
+    val tempText1 = "tempText1"
+
+    class Test1NestedClass{
+
+        fun testFun1(){
+            println("TestFun1")
+            // inner class와 다른점, 중첩 클래스에서는 외부 데이터에 있는 데이터 접근이 불가능하고
+            // inner class는 외부 데이터를 가져와서 사용이 가능하다.
+            println(tempText1)
+        }
+    }
+}
+
+class Test2 {
+
+    val tempText2 = "tempText2"
+
+	inner class Test2InnerClass{
+
+        fun testFun2(){
+            println("TestFun2")
+            println(tempText2)
+        }
+    }
+}
+
+*/
