d3c2bc7c693a8685ab04876b95490f3c994ebdac
hong_doc <holives7@gmail.com>
2021-12-29 01:29:41 +0900

    문법공부 제네릭

--- grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
+++ grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
@@ -240,3 +240,60 @@ fun test1() : Unit{
     println("abc")
 }
 */
+
+
+
+//제네릭(generic)
+//컴파일시에 타입 체크를 할 수 있다. -> 컴파일
+//타입 캐스팅이 필요없다.
+// 람다 / 고차함수 / 제네릭
+/*
+val box1 = Box1(10)
+println(box1.value)
+
+val box2 = Box2("숫자 10")
+println(box2.value)
+
+val box1_1 = Box1("10".toInt())
+println(box1_1.value)
+
+//제네릭
+val box3 = Box3(10)
+println(box3.value)
+
+val box4 = Box3("숫자 10")
+println(box4.value)
+
+//제네릭
+testFun1("abc")
+testFun2("a")
+testFun2(123)
+testFun3("a")
+testFun3(123)
+}
+
+//기존
+class Box1(test :Int){
+var value = test
+}
+
+class Box2(test : String){
+var value = test
+}
+
+// 제네릭
+class Box3<T>(test : T) {
+val value = test
+}
+//기존
+fun testFun1(a : String){
+println(a)
+}
+// 제네릭
+fun <T> testFun2(a : T){
+println(a)
+}
+fun <A> testFun3(a : A){
+println(a)
+}
+*/
