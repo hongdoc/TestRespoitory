7aab1b6d9579858f94c52ff992e144e8ea9f93b4
hong_doc <holives7@gmail.com>
2022-01-12 01:54:24 +0900

    kotlin scope function(run apply also)

--- grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
+++ grammar2/app/src/main/java/com/hongdoc/grammar2/MainActivity.kt
@@ -474,3 +474,62 @@ fun main() {
 }
 */
 
+fun main() {
+// kotlin scoe function
+// run
+//
+    /*
+    val service = multiPortService("www.naver.com", 80)
+    val result1 = service.query(service.prepareRequest() + " to port ${service.port}")
+
+    println(result1)
+
+    val result2 = service.run{
+        port = 8080
+        query(prepareRequest() + "to port $port")
+    }
+    println(result2)
+}
+
+class multiPortService(var url : String, var port : Int){
+    fun prepareRequest() : String = "기본 요청 url $url"
+    fun query(request : String) = "결과  query $request"
+    */
+
+    //apply
+    /*
+    val tester1 = Person("Tester1")
+    println(tester1)
+
+    tester1.age = 20
+    tester1.city = "Seoul"
+
+    println(tester1)
+
+    val tester2 = Person("Tester2").apply{
+        age = 21
+        city = "Incheon"
+    }
+    println(tester2)
+}
+	data class Person(
+    var name : String,
+    var age : Int = 0,
+    var city : String = ""
+    )
+
+    // also
+    val numbers = mutableListOf(1,2,3,4)
+    println("$numbers 여기에서 5를 추가합니다.")
+    numbers.add(5)
+    println(numbers)
+
+    val numbers2 = mutableListOf(1,2,3,4)
+    numbers2.also{
+        println("$numbers2 여기에서 5를 추가합니다.")
+    }.add(5)
+    println(numbers2)
+}
+    */
+
+
