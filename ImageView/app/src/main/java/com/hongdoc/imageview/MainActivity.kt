729709b9b984d17aa6ab67246830176bf55bb868
hong_doc <holives7@gmail.com>
2022-01-19 12:51:08 +0900

    ImageView std

---
+++ ImageView/app/src/main/java/com/hongdoc/imageview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.hongdoc.imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+주요 속성
+srcCompat : 보여줄 이미지를 지정한다. 안드로이드 4버전 까지는 src였는데 백터 방식의 이미지(SVG,PSD 등)을 처리할 수 있는
+기능을 추가하여 srcCompat로 변경하였다.
+
+주요 메서드
+setImageResource : res 폴더에 있는 이미지를 설정한다.
+setImageBitmap : Bitmap 객체로 만들어진 이미지를 설정한다.
+setImageDrawable : Drawable 객체로 만들어진 이미지를 설정한다.
+
+Drawabe
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
